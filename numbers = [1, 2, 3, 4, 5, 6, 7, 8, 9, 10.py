9e3c579fc8d3e6ca557c2c63879b3b88c397d20c
chmura <chmura100@vp.pl>
2025-02-06 12:04:59 +0100

    pierwszy commit

---
+++ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+squaters= []
+for number in numbers:
+    if number > 0:
+        squaters.append(number * number * number)
+print(squaters)
+
+#zadanie 2
+
+list = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
+for number in list:
+    if number == 0:
+        print(number) moze to TimeoutError
+
+#zadanie 2a
+
+numbers = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
+for number in numbers:
+    if number != 0:
+        print(number)   
+
+# no i mozna jeszcze: zera = [number for number in numbers if number == 0]      
